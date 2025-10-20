277ccbf58a13b5252ffef34647a09c1c72089967
Ömer Akdeniz <s149155@ap.be>
2025-10-20 22:27:40 +0200

    Kleine wijzigingen

--- MiniTextAdventure/Room.cs
+++ MiniTextAdventure/Room.cs
@@ -9,34 +9,84 @@ namespace MiniTextAdventure
     public enum Direction {North,East,South,West}
     public class Room
     {
-        public string Name { get; }
-        public string Description { get; }
-        public bool IsLethal { get; set; }
-        public bool RequiresItem { get; set; }
-        public string RequiredItemId { get; set; }
-        public bool HasMonster { get; set; }
-        public bool MonsterAlive { get; set; }
-        public Dictionary<Direction, Room> Exits { get; }
-        public List<string> Items { get; }
+        private string name;
+        public string Name 
+        {
+            get {  return name; } 
+        }
+
+        private string description;
+        public string Description 
+        {
+            get { return description; }
+        }
+
+        private bool isLethal;
+        public bool IsLethal 
+        
+        {
+            get { return isLethal;}
+            set { isLethal = value;}
+        }
+
+        private bool requiresItem;
+        public bool RequiresItem 
+        {
+            get { return requiresItem;}
+            set { requiresItem = value;}
+        }
+
+        private string requiredItemId;
+        public string RequiredItemId 
+        {
+            get { return requiredItemId;}
+            set { requiredItemId = value;}
+        }
+
+        private bool hasMonster;
+        public bool HasMonster 
+        {
+            get { return hasMonster;}
+            set {  hasMonster = value;}
+        }
+
+        public bool monsterAlive;
+        public bool MonsterAlive 
+        {
+            get { return monsterAlive;}
+            set { monsterAlive = value;}
+        }
+
+        private Dictionary<Direction, Room> exits;
+        public Dictionary<Direction, Room> Exits 
+        {
+            get { return  exits;}
+        }
+
+        private List<string> items;
+        public List<string> Items 
+        {
+            get { return items;}
+        }
 
         public Room(string name, string description)
         {
-            Name = name;
-            Description = description;
-            Exits = new Dictionary<Direction, Room>();
-            Items = new List<string>();
+            this.name = name;
+            this.description = description;
+            exits = new Dictionary<Direction, Room>();
+            this.items = new List<string>();
         }
 
-        public void Connect(Direction dir, Room target)
+        public void Connect(Direction direction, Room target)
         {
-            Exits[dir] = target;
+            Exits[direction] = target;
         }
 
-        public Room GetExit(Direction dir)
+        public Room GetExit(Direction direction)
         {
-            if (Exits.ContainsKey(dir))
+            if (Exits.ContainsKey(direction))
             {
-                return Exits[dir];
+                return Exits[direction];
             }
             else
             {
