907a65ffaa24754fbffee241c556d49191541788
dzhanerahmed3 <dzhaner.ahmedd@gmail.com>
2025-10-27 16:14:28 +0100

    item en inventory klasse toegevoegd

---
+++ MiniTextAdventure/Inventory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Text_Adventure
+{
+    internal class Inventory
+    {
+        private List<Item> items = new List<Item>();
+
+        public void Add(Item item)
+        {
+            if (item != null) return;
+            bool bestaat = false;
+
+            foreach(Item i in items)
+            {
+                if(i.Id == item.Id)
+                {
+                    bestaat = true;
+                    break;
+                }
+
+            }
+
+            if (!bestaat)
+            {
+                items.Add(item);
+            }
+            else
+            {
+                Console.WriteLine("Deze item zit al in je inventaris");
+            }
+        }
+
+        public void Remove(Item item)
+        {
+            Item teVerwijderen = null;
+            foreach (Item i in items)
+            {
+                if(item.Id == i.Id)
+                {
+                    teVerwijderen = i;
+                    items.Remove(item);
+                    Console.WriteLine(item.Name + " verwijderd van je inventaris");
+                }
+                if (teVerwijderen != null)
+                {
+                    items.Remove(teVerwijderen);
+                }
+            }
+        }
+        public bool HasItem(string id)
+        {
+            bool hasIt = false;
+
+            foreach (Item i in items)
+            {
+                if(i.Id == id)
+                {
+                    hasIt = true;
+                }
+            }
+
+            return hasIt;
+        }
+        public string ListItems()
+        {
+            string lijstItems = "";
+            if(items.Count == 0)
+            {
+                return "Je inventaris is leeg";
+            }
+            string begin = "Items in je inventaris: \n";
+            foreach (Item i in items)
+            {
+                lijstItems += "- " + i.Name + "\n";
+            }
+
+            return begin + lijstItems;
+        }
+    }
+}
---
+++ MiniTextAdventure/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Text_Adventure
+{
+    internal class Item
+    {
+		private string id;
+
+		public string Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string description;
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+
+		public Item(string id1,string name1,string description1)
+		{
+			id = id1;
+			name = name1; 
+			description = description1;
+		}
+
+        public override string ToString()
+        {
+            return $"{Name}: {Description}";
+        }
+
+
+	}
+}
