0882e77d4e7cce4da03aec00fad6299831b2066f
Hayatiii40 <hayatiiibjk@gmail.com>
2025-10-30 16:33:52 +0100

    Full version combat systeem + monster + tests

---
+++ MiniTextAdventure/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTextAdventure
+{
+    public class Item
+    {
+        public string Id;
+        public string Description;
+
+        public Item(string id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
